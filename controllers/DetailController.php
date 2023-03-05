1b72eec7abbffcb9431fcf79bc8a6492070b1875
NguyenVanCong2051063627 <99166356+NguyenVanCong2051063627@users.noreply.github.com>
2023-03-05 23:08:30 +0700

    category_home

---
+++ controllers/DetailController.php
@@ -0,0 +1,9 @@
+<?php
+    require("services/ArticleService.php");
+    class DetailController{
+        public function index($id){
+            $articelService = new ArticleService();
+            $articles = $articelService->getDetailArticle($id);
+            include("view/home/detail.php");
+        }
+    }
--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -1,12 +1,11 @@
 <?php
-include("services/ArticleService.php");
-class HomeController{
-    // Hàm xử lý hành động index
-    public function index(){
-        // Nhiệm vụ 1: Tương tác với Services/Models
-        $articelService = new ArticleService();
-        $articles = $articelService->getAllArticles();
-        // Nhiệm vụ 2: Tương tác với View
-        include("views/home/index.php");
+    require("services/ArticleService.php");
+    class HomeController{
+        public function index(){
+            $articelService = new ArticleService();
+            $articles = $articelService->getAllArticlesHome();
+
+            include("view/home/index.php");
+        }
+
     }
-}
